76e0fff2dc5d1392f55b075b6e93f64e0aa98126
Maxime Britto <maxime@purplegiraffe.fr>
2020-04-10 19:45:28 +0200

    191_Preparer_notre_code_pour_la_production

--- todos/Sources/App/configure.swift
+++ todos/Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import Fluent
 import FluentSQLiteDriver
+import FluentMySQLDriver
 import Vapor
 
 // configures your application
@@ -7,7 +8,13 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    if app.environment == .production {
+        let password = Environment.get("MYSQL_PASSWORD")!
+        //export MYSQL_PASSWORD=3YroPhon=h9PzWM}dcwf
+        app.databases.use(.mysql(hostname: "127.0.0.1", username: "vapor_user", password: password))
+    } else {
+        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    }
 
     app.migrations.add(CreateTodo())
     app.migrations.add(CreateUser())
