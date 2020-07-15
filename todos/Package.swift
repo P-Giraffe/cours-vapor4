50c875ad0128fb3f1eb25bce2bbd327914d69cc2
Anthony Fassler <anthonyfassler1@gmail.com>
2020-07-15 22:36:15 +0200

    Update to Vapor 4.20.1 and sqlite-driver to 4.0.0-rc2.1

--- todos/Package.swift
+++ todos/Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.15.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.20.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc.2.1"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-rc.2")
     ],
     targets: [
