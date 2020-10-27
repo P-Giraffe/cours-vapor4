9081299de6abc8b93d63536123a2187dd5cf2a73
Anthony Fassler <anthonyfassler1@gmail.com>
2020-10-27 21:44:10 +0100

    Update to Vapor 4.34.0

--- todos/Package.swift
+++ todos/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.31.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.34.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0")
